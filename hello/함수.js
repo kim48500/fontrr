f85a6fa76b218443868704f0645becf664ab057e
kim singjin <ksj48500@naver.com>
2025-06-10 13:13:17 +0900

    자바스트립트

---
+++ hello/함수.js
@@ -0,0 +1,15 @@
+const a = 1;
+const b = 2;
+const c = 100;
+
+function functionName(x, y) {
+    return x / y;
+
+};
+console.log(functionName(c, b));
+//화살표
+const arrowFunction = (x, y) => {
+    return x * y;
+
+};
+console.log(arrowFunction(b, c));
